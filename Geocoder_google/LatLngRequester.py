75a3383e5c509ad238b1709a9f6c7845506f2a93
FlorianFusseder-Desktop <florianfueder@yahoo.de>
2016-11-30 13:01:15 +0100

    Simplifyed LatLng search for single Addresses

--- Geocoder_google/LatLngRequester.py
+++ Geocoder_google/LatLngRequester.py
@@ -1,23 +1,45 @@
-import sys
 import googlemaps
 
+
 def getInput():
-    return input("Please enter address: ") 
+    return input("Please enter address: ")
+
+def getAllKeys():
+    L = []
+    with open("./key.txt", "r") as keylist:
+        for line in keylist:
+            currentLine = line.split("\t")[0]
+            try:
+                gmaps = googlemaps.Client(key=currentLine)
+                gmaps.geocode("Regensburg, Germany")
+                L.append(currentLine)
+            except:
+                print("key " + currentLine + " not working... skipping")
+                continue
+
+    if len(L) > 0:
+        print(str(len(L)) + " keys Working")
+    else:
+        exit()
+    return L
+
 
 def main():
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "-?" or sys.argv[1] == "-help":
-            print("Start with google api key as Parameter, to end enter exit")
-        else:
-            gmaps = googlemaps.Client(key=sys.argv[1])
+    counter = 0
+    L = getAllKeys()
+    gmaps = googlemaps.Client(key=L[counter])
+    while True:
+        try:
             address = getInput()
-            while address != "exit":
-                response = gmaps.geocode(address)
-                print(response[0]['geometry']['location'])
-                address = getInput()
-    else:
-        print("Wrong Parameters to start. Check -help or -?")
+            if address == "exit":
+                exit()
+            response = gmaps.geocode(address)
+            print(response[0]['geometry']['location'])
+            address = getInput()
+        except:
+            counter +=1
+            gmaps = googlemaps.Client(key=L[counter])
 
 
-
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
